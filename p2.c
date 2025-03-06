f467e0bb6ab1ed2e6df7e5617ab0d573ce343298
Darshan <130634221+Darshanspace@users.noreply.github.com>
2025-03-06 09:23:48 +0000

    created C file

---
+++ p2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    int num1 = 0;
+    long int num2 = 0;
+    long long int num3 = 0;
+    double d = 0.0;
+    float f = 0.0f;
+    printf("%d %d %d %d %d", (int)sizeof(num1), (int)sizeof(num2), (int)sizeof(num3), (int)sizeof(d), (int)sizeof(f));
+}
