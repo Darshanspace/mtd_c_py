f039d92e85ca9ccaa33c065ec7e7a6d8e0a2c16b
Darshanspace <ashdarshan9711@gmail.com>
2025-03-07 11:03:51 +0530

    created for learning

--- p2.c
+++ p2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void main()
+int main()
 {
     int num1 = 0;
     long int num2 = 0;
@@ -7,4 +7,5 @@ void main()
     double d = 0.0;
     float f = 0.0f;
     printf("%d %d %d %d %d", (int)sizeof(num1), (int)sizeof(num2), (int)sizeof(num3), (int)sizeof(d), (int)sizeof(f));
+   
 }
