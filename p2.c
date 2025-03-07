9b927c00649dd935c1767b3ebe1e3088a326a799
Darshanspace <ashdarshan9711@gmail.com>
2025-03-07 11:06:04 +0530

    pdf

--- p2.c
+++ p2.c
@@ -6,6 +6,6 @@ int main()
     long long int num3 = 0;
     double d = 0.0;
     float f = 0.0f;
-    printf("%d %d %d %d %d", (int)sizeof(num1), (int)sizeof(num2), (int)sizeof(num3), (int)sizeof(d), (int)sizeof(f));
+    printf("%d %d %d %d %d,", (int)sizeof(num1), (int)sizeof(num2), (int)sizeof(num3), (int)sizeof(d), (int)sizeof(f));
    
 }
